2d269ae2daac3cfc47d44d0bc6ad70c47e0f1721
Roger Hutchings <roger@hutchin.gs>
2019-03-04 16:57:19 +0000

    add analyzer

--- packages/testapp/next.config.js
+++ packages/testapp/next.config.js
@@ -1,6 +1,20 @@
 const path = require('path')
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
 
-module.exports = {
+module.exports = withBundleAnalyzer({
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html'
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html'
+    }
+  },
+
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
@@ -11,4 +25,4 @@ module.exports = {
 
     return config
   }
-}
+})
