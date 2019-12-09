d8d1a8ec383f298b6cc0f0ec437f40591207da63
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2019-12-09 13:56:09 +0100

    added a babel emotion playground

--- packages/next-app/pages/index.js
+++ packages/next-app/pages/index.js
@@ -3,16 +3,22 @@ import Foo from '@zooniverse/grommet-lib';
 import Bar from '@zooniverse/styled-components-lib';
 import { CssButton } from '@zooniverse/emotions-lib';
 // import { StyleButton } from '@zooniverse/emotions-lib';
+import { CssButton as BabelCssButton } from '@zooniverse/emotions-babel-lib';
+// import { StyleButton as BabelStyleButton } from '@zooniverse/emotions-babel-lib';
 
 export default function () {
   console.log(CssButton)
   console.log(Bar)
   // console.log(StyleButton);
+  console.log(BabelCssButton);
+  // console.log(BabelStyleButton);
   return (
     <>
       <Bar /> 
       <CssButton />
       {/* <StyleButton/> */}
+      <BabelCssButton />
+      {/* <BabelStyleButton/> */}
     </>
   )
 }
