116c788b9012c5ee0925790b449522c62f4e63e5
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2019-12-09 13:38:19 +0100

    uncomment styleButton

--- packages/next-app/pages/index.js
+++ packages/next-app/pages/index.js
@@ -1,17 +1,18 @@
 import { Box, Grommet } from 'grommet';
 import Foo from '@zooniverse/grommet-lib';
 import Bar from '@zooniverse/styled-components-lib';
-import { CssButton, StyleButton } from '@zooniverse/emotions-lib';
+import { CssButton } from '@zooniverse/emotions-lib';
+// import { StyleButton } from '@zooniverse/emotions-lib';
 
 export default function () {
   console.log(CssButton)
-  console.log(StyleButton)
   console.log(Bar)
+  // console.log(StyleButton);
   return (
     <>
       <Bar /> 
       <CssButton />
-      <StyleButton />
+      {/* <StyleButton/> */}
     </>
   )
 }
