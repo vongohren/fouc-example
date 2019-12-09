dbc203bf3fac780a85c22a88c242be4171f1ffe2
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2019-12-09 13:35:44 +0100

    Minor tweaks and added emotion

---
+++ packages/emotions-lib/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const plugins = [
+  new CleanWebpackPlugin(),
+]
+
+module.exports = {
+  devtool: 'hidden-source-map',
+  entry: './src/index.js',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+    library: 'testlib',
+  },
+  plugins,
+}
--- packages/next-app/pages/index.js
+++ packages/next-app/pages/index.js
@@ -1,15 +1,17 @@
-import { Box, Grommet } from 'grommet'
-import Foo from '@zooniverse/grommet-lib'
-import Bar from '@zooniverse/styled-components-lib'
+import { Box, Grommet } from 'grommet';
+import Foo from '@zooniverse/grommet-lib';
+import Bar from '@zooniverse/styled-components-lib';
+import { CssButton, StyleButton } from '@zooniverse/emotions-lib';
 
 export default function () {
+  console.log(CssButton)
+  console.log(StyleButton)
+  console.log(Bar)
   return (
-    <Grommet>
-      <Box margin='medium'>
-        Welcome to the test app
-        <Foo />
-        <Bar />
-      </Box>
-    </Grommet>
+    <>
+      <Bar /> 
+      <CssButton />
+      <StyleButton />
+    </>
   )
 }
