6c2450b29f106cdbae5ecf3dc4ecd9e7f7b92cd1
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2019-12-22 18:44:49 +0100

    remove static storybook

--- packages/tsdx-styled-comps-lib/storybook-static/main.757d6c092cd84084b71e.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(409),c(563),n.exports=c(851)},1:function(n,o){},563:function(n,o,c){"use strict";c.r(o);c(564),c(848)}},[[0,1,2]]]);
